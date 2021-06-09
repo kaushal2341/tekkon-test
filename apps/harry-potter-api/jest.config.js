a34133b2e3c6b8207e784e424ae9f7620c96a87b
Kaushal Regmi <kaushal.oksoftintl@gmail.com>
2021-06-09 16:56:45 +0545

    initial commit

---
+++ apps/harry-potter-api/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'harry-potter-api',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/harry-potter-api',
+};
